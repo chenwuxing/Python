295b5c5f2136d7fb42a74a4b480cd6e156ce03d4
chenwuxing <chenwuxing87@163.com>
2019-02-22 10:29:14 +0800

    添加计算运行时间功能

--- traverse.py
+++ traverse.py
@@ -2,6 +2,15 @@ import os
 import cv2
 
 
+def timer(func):
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        func(*args,**kwargs)
+        stop_time = time.time()
+        print("处理图片共耗时:{}s".format(stop_time-start_time))
+    return wrapper
+
+@timer
 def traverse_files(outer_file): #   outer_file的形式应该像'D:/a/'
     for inner_files in os.listdir(outer_file):  # 此时可以得到包含内层文件夹名称的列表
         inner_files_path = outer_file + inner_files + '/'   #得到内部文件夹的路径
@@ -10,6 +19,8 @@ def traverse_files(outer_file): #   outer_file的形式应该像'D:/a/'
             img = cv2.imread(img_path)
             res = cv2.resize(img,(64,64),interpolation = cv2.INTER_CUBIC)
             cv2.imwrite('G:/test/' + inner_files + '_' + item,res)
+
+
 
 
 
