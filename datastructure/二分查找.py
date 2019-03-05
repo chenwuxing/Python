175f745b7c5a8371476123838127609012bb0a91
chenwuxing <chenwuxing87@163.com>
2019-03-05 18:00:12 +0800

    二分查找

---
+++ datastructure/二分查找.py
@@ -0,0 +1,35 @@
+def binary_search(list,item):
+    first = 0
+    last = len(list)-1
+    found = False 
+    while first <= last and not found:
+        mid_pos = (first + last) // 2
+        if list[mid_pos] == item:
+            found = True
+        else:
+            if item < list[mid_pos]:
+                last = mid_pos - 1
+            else:
+                first = mid_pos + 1
+    return found
+
+#   递归实现
+def recursion_binary_search(list,item):
+    if len(list) == 0:
+        return False
+    else:
+        mid_pos = len(list) // 2
+        if list[mid_pos] == item:
+            return True
+        else:
+            if item < list[mid_pos]:
+                return recursion_binary_search(list[:mid_pos],item)
+            else:
+                return recursion_binary_search(list[mid_pos+1:],item)
+
+print(recursion_binary_search([1,5,7,15,69,70,111],69))
+
+    
+
+
+
