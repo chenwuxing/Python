a18ee4e6d974d515a2db73953f993572854ef852
chenwuxing <chenwuxing87@163.com>
2019-03-20 11:19:21 +0800

    需要认真思考的，递归

---
+++ leetcode/easy/100.same-tree.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode id=100 lang=python3
+#
+# [100] Same Tree
+#
+# https://leetcode.com/problems/same-tree/description/
+#
+# algorithms
+# Easy (49.53%)
+# Total Accepted:    355.7K
+# Total Submissions: 717.9K
+# Testcase Example:  '[1,2,3]\n[1,2,3]'
+#
+# Given two binary trees, write a function to check if they are the same or
+# not.
+# 
+# Two binary trees are considered the same if they are structurally identical
+# and the nodes have the same value.
+# 
+# Example 1:
+# 
+# 
+# Input:     1         1
+# ⁠         / \       / \
+# ⁠        2   3     2   3
+# 
+# ⁠       [1,2,3],   [1,2,3]
+# 
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:     1         1
+# ⁠         /           \
+# ⁠        2             2
+# 
+# ⁠       [1,2],     [1,null,2]
+# 
+# Output: false
+# 
+# 
+# Example 3:
+# 
+# 
+# Input:     1         1
+# ⁠         / \       / \
+# ⁠        2   1     1   2
+# 
+# ⁠       [1,2,1],   [1,1,2]
+# 
+# Output: false
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self,p,q):
+        if p == None and q == None:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        return False
+
+        
+
---
+++ leetcode/easy/101.symmetric-tree.py
@@ -0,0 +1,66 @@
+#
+# @lc app=leetcode id=101 lang=python3
+#
+# [101] Symmetric Tree
+#
+# https://leetcode.com/problems/symmetric-tree/description/
+#
+# algorithms
+# Easy (42.85%)
+# Total Accepted:    369K
+# Total Submissions: 860.7K
+# Testcase Example:  '[1,2,2,3,4,4,3]'
+#
+# Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+# around its center).
+# 
+# 
+# For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠/ \ / \
+# 3  4 4  3
+# 
+# 
+# 
+# But the following [1,2,2,null,3,null,3]  is not:
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠  \   \
+# ⁠  3    3
+# 
+# 
+# 
+# 
+# Note:
+# Bonus points if you could solve it both recursively and iteratively.
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def help(self,p,q):
+        if p == None and q == None:
+            return True
+        if p and q and p.val == q.val:
+            return self.help(p.left,q.right) and self.help(p.right,q.left)
+        return False
+
+    def isSymmetric(self, root):
+        if root :
+            return self.help(root.left,root.right)
+
+        
+        return True
+
+        
+
