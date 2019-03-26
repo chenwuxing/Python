0474e20dc2da0c3c2d529e5d6437dee68d9c64aa
chenwuxing <chenwuxing87@163.com>
2019-03-26 10:37:47 +0800

    ***

---
+++ leetcode/easy/283.move-zeroes.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=283 lang=python3
+#
+# [283] Move Zeroes
+#
+# https://leetcode.com/problems/move-zeroes/description/
+#
+# algorithms
+# Easy (53.75%)
+# Total Accepted:    435.5K
+# Total Submissions: 809K
+# Testcase Example:  '[0,1,0,3,12]'
+#
+# Given an array nums, write a function to move all 0's to the end of it while
+# maintaining the relative order of the non-zero elements.
+# 
+# Example:
+# 
+# 
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# 
+# Note:
+# 
+# 
+# You must do this in-place without making a copy of the array.
+# Minimize the total number of operations.
+# 
+#
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j],nums[i] = nums[i],nums[j]
+                j += 1
+
+
+
+        
+
