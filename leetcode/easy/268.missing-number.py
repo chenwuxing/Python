5015bb98c4ce0ce83593f52992332dc2abd497db
chenwuxing <chenwuxing87@163.com>
2019-03-26 11:22:30 +0800

    **

---
+++ leetcode/easy/268.missing-number.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+# https://leetcode.com/problems/missing-number/description/
+#
+# algorithms
+# Easy (47.67%)
+# Total Accepted:    255.5K
+# Total Submissions: 534.8K
+# Testcase Example:  '[3,0,1]'
+#
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find
+# the one that is missing from the array.
+# 
+# Example 1:
+# 
+# 
+# Input: [3,0,1]
+# Output: 2
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [9,6,4,2,3,5,7,0,1]
+# Output: 8
+# 
+# 
+# Note:
+# Your algorithm should run in linear runtime complexity. Could you implement
+# it using only constant extra space complexity?
+#
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return n*(n+1)//2 - sum(nums)
+
+
+
+
+        
+
