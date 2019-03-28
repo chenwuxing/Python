3440680360a0b8672893609d22d717b2d6ab826e
chenwuxing <chenwuxing87@163.com>
2019-03-28 19:58:39 +0800

    ***

---
+++ leetcode/easy/476.number-complement.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=476 lang=python3
+#
+# [476] Number Complement
+#
+# https://leetcode.com/problems/number-complement/description/
+#
+# algorithms
+# Easy (62.10%)
+# Total Accepted:    102.3K
+# Total Submissions: 164.7K
+# Testcase Example:  '5'
+#
+# Given a positive integer, output its complement number. The complement
+# strategy is to flip the bits of its binary representation.
+# 
+# Note:
+# 
+# The given integer is guaranteed to fit within the range of a 32-bit signed
+# integer.
+# You could assume no leading zero bit in the integer’s binary
+# representation.
+# 
+# 
+# 
+# Example 1:
+# 
+# Input: 5
+# Output: 2
+# Explanation: The binary representation of 5 is 101 (no leading zero bits),
+# and its complement is 010. So you need to output 2.
+# 
+# 
+# 
+# Example 2:
+# 
+# Input: 1
+# Output: 0
+# Explanation: The binary representation of 1 is 1 (no leading zero bits), and
+# its complement is 0. So you need to output 0.
+# 
+# 
+#
+class Solution:
+    def findComplement(self, num: int) -> int:
+        i = 1
+        while i <= num:
+            num ^= i
+            i <<= 1
+        return num
+        
+        
+
